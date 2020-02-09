e458c53a26ee67de93312c7b9ab1122f1d35fe3b
SolaGbenro <Solagbenro1@gmail.com>
2020-02-08 18:05:25 -0800

    Completed the print_board and find_empty methods which return the board and a touple containing the indexes of the first found empty spot starting from top left going to bottom right

--- working.py
+++ working.py
@@ -15,11 +15,35 @@ def print_board(board):
     for i in range(len(board)):
         # sudoku board is made up of 3x3 squares
         # this is to break up rows
-        if i % 3 and i != 0:
-            print('- - - - - - - - - - - - - ')
+        if i % 3 == 0 and i != 0:
+            print('- - - - - - - - - - - -')
 
         # board[0] = len of rows
         for j in range(len(board[0])):
             # checking to make sure it is not the 0th colum
             # 0 % 3 === 0 and would print out on the edge
             if j % 3 == 0 and j != 0:
+                print(" | ", end="") # as opposed to default line ending of \n
+            
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+#print_board(board)
+
+"""
+    Given a board, find empty square. empty = 0
+"""
+def find_empty(board):
+    # iterate through the rows
+    for i in range(len(board)):
+        # iterate through the columns
+        for j in range(len(board[0])):
+            # if the number in the space is 'empty' (0) return it
+            if board[i][j] == 0:
+                return (i, j)   # row, col
+            
+#print(find_empty(board))
+
+
